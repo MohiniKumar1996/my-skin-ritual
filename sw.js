8acdd8d5eb4a369d47eefb1926a6f7d5435a91f9
MohiniKumar1996 <mohinikumar96@gmail.com>
2026-03-07 18:45:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'skin-ritual-v1';
+const CACHE_NAME = 'skin-ritual-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 // ---- Install & Cache ----
